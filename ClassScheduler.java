c5e81c6f797226abb23dbf0c2a4a9ed0c8d7061f
Hartman, Matthew <matthew.hartman@cmegroup.com>
2016-01-26 10:05:44 -0600

    Initial commit

---
+++ ClassScheduler.java
@@ -0,0 +1,15 @@
+/**
+ * You are responsible for scheduling a series of training courses for your
+ * employees. Each employee will provide you a list of available dates that they
+ * can attend. Write a class to return a list of the minimum number of dates
+ * needed so that all employees attend once.
+ * <p/>
+ * Sample Data Set
+ * [[1,2,5], [2,3,7], [5,7,9]] -> [2,5]
+ */
+public class ClassScheduler {
+
+    public Set<Integer> findValidTrainingDays(List<Set<Integer>> employeeAvailability) {
+        return null;
+    }
+}
